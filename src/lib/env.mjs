6a02275e6beeaa6cce85097db4debf667a8cf102
Joshua <joshua.tanri@sevima.id>
2024-04-03 11:35:05 +0700

    refactor: adjust env

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -3,18 +3,20 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
+    POSTGRES_PRISMA_URL: z.string().url(),
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
-    OPENAI_KEY: z.string().min(1),
+    OPENAI_API_KEY: z.string().optional(),
+    OPENAI_API_URL: z.string().url().optional(),
   },
   client: {
     // Public env
   },
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    OPENAI_KEY: process.env.OPENAI_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    OPENAI_API_URL: process.env.OPENAI_API_URL,
   },
 });
